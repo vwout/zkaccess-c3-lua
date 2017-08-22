013cdaf3e30a512f03264b89ab9fe6993ba01108
vwout <github15@info.vwout.nl>
2017-08-22 21:18:42 +0200

    Remove reverse_array function

--- source/C3_test.lua
+++ source/C3_test.lua
@@ -17,7 +17,6 @@ end
 
 function test_c3_time_decode()
   assert_equal(os.time({year=2017, month=07, day=30, hour=15, min=24, sec=32}), C3.byte_array_to_time({0x21, 0xad, 0x99, 0x30}))
-  assert_equal(os.time({year=2017, month=07, day=30, hour=15, min=24, sec=32}), C3.byte_array_to_time(reverse_array({0x30, 0x99, 0xad, 0x21})))
   assert_equal(os.time({year=2013, month=10, day=8, hour=14, min=38, sec=32}),  C3.byte_array_to_time({0x1a, 0x61, 0x70, 0xe8}))
 end
 
--- source/utils.lua
+++ source/utils.lua
@@ -1,10 +1,10 @@
 -- Various conversion utilities
 
 -- convert string to table of bytes
-function str_to_arr(s, arr)
+function str_to_arr(str, arr)
   local data = arr or {}
-  for i = 1, #s do 
-    data[#data + 1] = string.byte(s, i)
+  for i = 1, #str do 
+    data[#data + 1] = string.byte(str, i)
   end
   return data
 end
@@ -16,15 +16,4 @@ function arr_to_str(bytes, str)
     data = data .. string.char(v)
   end
   return data
-end
-
--- reverse the ordering of a sorted array (table)
-function reverse_array(arr)
-  local rev_arr = {}
-  
-  for i,v in ipairs(arr) do
-    rev_arr[#arr - i + 1] = v
-  end
-  
-  return rev_arr
 end
