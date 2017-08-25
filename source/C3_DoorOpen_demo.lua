715cbfad82cf3f2c3e16f586dcba6b473a5e9468
vwout <github15@info.vwout.nl>
2017-08-25 23:02:56 +0200

    Add sample application C3_DoorOpen_demo.lua to open and close doors

---
+++ source/C3_DoorOpen_demo.lua
@@ -0,0 +1,59 @@
+#!/usr/bin/env lua5.1
+
+local C3 = require("c3.c3")
+local consts = require("C3.consts")
+
+if #arg < 3 or #arg > 4 then
+  print "Usage: C3_RTLog_demo ip door,door,... open|close [duration=180]"
+  os.exit(0)
+end
+
+local c3_ip = arg[1]
+
+local c3_doors = {}
+for door in string.gmatch(arg[2] or "", "([^,]+)") do
+  local idoor = tonumber(door)
+  if idoor then
+    table.insert(c3_doors, idoor)
+  end
+end
+
+local c3_open = nil
+if arg[3] == "open" then
+  c3_open = true
+elseif arg[3] == "close" then
+  c3_open = false
+end
+
+local c3_duration = tonumber(arg[4]) or 180
+
+if c3_ip and next(c3_doors) and c3_open ~= nil then
+  C3.setDebug(false)
+
+  print("Connecting to " .. c3_ip .. " ... ")
+  local connected, err = C3.connect(c3_ip)
+  if connected then
+    if not c3_open then
+      c3_duration = 0
+    end
+    if c3_duration > 254 then
+      c3_duration = 254
+    end
+
+    for _,door in ipairs(c3_doors) do
+      if c3_open then
+        print(string.format("Open door %d for %d seconds.", door, c3_duration))
+      else
+        print(string.format("Close door %d.", door))
+      end
+
+      local doorOpenOperation = C3.ControlDeviceOutput(door, consts.C3_CONTROL_OUTPUT_ADDRESS_DOOR_OUTPUT, c3_duration)
+      C3.controlDevice(doorOpenOperation)
+    end
+
+    C3.disconnect()
+    print("Disconnected.")
+  else
+    error("Connection failed:" .. err, 0)
+  end
+end
--- source/C3_RTLog_demo.lua
+++ source/C3_RTLog_demo.lua
@@ -3,41 +3,43 @@
 local socket = require("socket")  -- for having a sleep function
 local C3 = require("c3.c3")
 
-local c3_ip = arg[1]
-if c3_ip == nil then
-  print "Program requires one argument: IP address of the C3 device."
+if #arg ~= 1 then
+  print "Usage: C3_RTLog_demo ip"
   os.exit(0)
 end
 
-C3.setDebug(false)
+local c3_ip = arg[1]
+if c3_ip then
+  C3.setDebug(false)
 
-print("Connecting to " .. c3_ip .. " ... ")
-local connected, err = C3.connect(c3_ip)
-if connected then
-  print("SessionId: " .. C3.getSessionId())
-  print("Press Ctrl-C to stop.")
+  print("Connecting to " .. c3_ip .. " ... ")
+  local connected, err = C3.connect(c3_ip)
+  if connected then
+    print("SessionId: " .. C3.getSessionId())
+    print("Press Ctrl-C to stop.")
 
-  while true do
-    local has_da_status = false
+    while true do
+      local has_da_status = false
 
-    local rtlogs = C3.getRTLog()
-    print("Received RT logs:" .. #rtlogs)
+      local rtlogs = C3.getRTLog()
+      print("Received RT logs:" .. #rtlogs)
 
-    for n,rtlog in pairs(rtlogs) do
-      has_da_status = has_da_status or rtlog.is_dastatus()
-      print(n)
-      rtlog.print()
-    end
+      for n,rtlog in pairs(rtlogs) do
+        has_da_status = has_da_status or rtlog.is_dastatus()
+        print(n)
+        rtlog.print()
+      end
 
-    if has_da_status then
-      if not pcall(socket.sleep, 15) then
-        break
+      if has_da_status then
+        if not pcall(socket.sleep, 15) then
+          break
+        end
       end
     end
-  end
 
-  C3.disconnect()
-  print("Disconnected.")
-else
-  error("Connection failed:" .. err, 0)
+    C3.disconnect()
+    print("Disconnected.")
+  else
+    error("Connection failed:" .. err, 0)
+  end
 end
